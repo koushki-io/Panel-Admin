9445d2c1382a08332c59e17e3db90dd4e5d938ee
mehdi <mkoushki.79@gmail.com>
2022-08-15 02:03:46 -0700

    add recharts

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import { UserData } from '../../../data'
+import Chart from '../../chart/Chart'
 import Featuredinfo from '../../featuredinfo/Featuredinfo'
+
 import './home.css'
+
 
 const Home = () => {
   return (
     <div className="Home">
       <Featuredinfo/>
+      <Chart  title="User Analytics"  UserData={UserData} dataKey1="uv" dataKey2="pv" dataKey3="amt"  grid/>
+
     </div>
   )
 }
